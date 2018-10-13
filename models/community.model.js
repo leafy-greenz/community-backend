2d4125b4b93fa926a5246ee27cd4ac7b10cf34ed
Greg Bahr <gregsbahr@gmail.com>
2018-10-13 18:13:48 -0500

    added location to community

--- models/community.model.js
+++ models/community.model.js
@@ -7,6 +7,7 @@ const Tag = require('./tag.model');
 const communitySchema = new mongoose.Schema({
     name: String,
     description: String,
+    location: String,
     members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     events: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Event' }],
     announcements: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Announcement' }],
