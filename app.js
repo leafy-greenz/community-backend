8459707791ba4858724e10eeeff52f2f53fac4e5
Greg Bahr <gregsbahr@gmail.com>
2018-10-12 21:52:46 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+const dbConfig = require('./config/db.config');
+const cors = require('cors');
+
+mongoose.connect(dbConfig.uri, () => {
+    useMongoClient: true
+});
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+mongoose.connection.once('open', function() {
+    console.log("Connected to database: " + dbConfig.uri);
+});
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(cors());
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.send({
+        message: err.message,
+        error: err
+    })
+});
+
+module.exports = app;
